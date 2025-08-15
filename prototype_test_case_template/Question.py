26859b267722aab691abf67257bc2276a816f626
John S Thanyanop <156885586+SThanyanop@users.noreply.github.com>
2025-08-16 01:15:01 +0700

    Add files via upload

---
+++ prototype_test_case_template/Question.py
@@ -0,0 +1,12 @@
+# Question name
+
+'''
+poblem here
+'''
+
+# Student do exercise here
+# do not change the function name
+# do not change the function input
+
+def Question(input):
+    return input
