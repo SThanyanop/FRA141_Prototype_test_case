e50d4819249200fac05cae5db64880de809836a2
John S Thanyanop <156885586+SThanyanop@users.noreply.github.com>
2025-08-16 13:29:54 +0700

    Set folder for working on test use

---
+++ prototype_test_case_template/QuizQuestion/Question.py
@@ -0,0 +1,12 @@
+# Question name
+
+'''
+poblem here
+'''
+
+# Student do quiz here below
+# do not change the function name
+# do not change the function input
+
+def Question(input):
+    return input
